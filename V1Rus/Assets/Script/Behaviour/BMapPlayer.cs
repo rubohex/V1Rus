d257296bb3df75250e69cb10f67db0cd9034c502
Iñaki Hulsman <inaruiz600@gmail.com>
2020-02-09 20:51:18 +0100

    Inicio del Minimapa
    fbx de minimapa + comienzo con algunos scripts.
    Distintos scripts para jugador, cámara y tablero de minimapa a los de nivel

---
+++ V1Rus/Assets/Script/Behaviour/BMapPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BMapPlayer : MonoBehaviour
+{
+    #region ATRIBUTES
+    [Header("Data")]
+
+    /// Datos de la Camara
+    public DPlayerInfo playerInfo;
+
+    /// Codigo de tecla para rotar -90º
+    private KeyCode rotateLeft;
+    /// Codigo de tecla para rotar -90º
+    private KeyCode rotateRight;
+    /// Codigo de tecla para mover hacia arriba
+    private KeyCode move;
+
+    /// Velocidad de rotacion del jugador
+    private float rotationSpeed;
+    /// Velocidad de movimiento del jugador
+    private float moveSpeed;
+
+    /// Indica si el jugador esta rotando o moviendose
+    private bool isMoving = false;
+
+    /// Diccionario para guardar la relacion entre la rotacion y la casilla a la que apuntamos
+    private Dictionary<int, int> direction = new Dictionary<int, int>();
+
+
+    #endregion
+
+    #region METHODS
+
+    private void Awake()
+    {
+        rotateLeft = playerInfo.rotateLeft;
+        rotateRight = playerInfo.rotateRight;
+        move = playerInfo.move;
+        rotationSpeed = playerInfo.rotationSpeed;
+        moveSpeed = playerInfo.moveSpeed;
+    }
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
---
+++ V1Rus/Assets/Script/Data/Script/DMapInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "MapInfo", menuName = "ScriptableObjects/MapInfo", order = 1)]
+
+public class DMapInfo : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
