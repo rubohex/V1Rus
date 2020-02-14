c98d30d61adb8c6f702951ece6df9b8d572d58e0
Alvaro-Donamaria <60362583+Alvaro-Donamaria@users.noreply.github.com>
2020-02-14 23:58:04 +0100

    Implementacion de terminal y pantalla
    En la escena del nivel 1 he añadido un terminal con su pantalla y he programado el movimiento de la pantalla

---
+++ V1Rus/Assets/Script/Behaviour/BTerminal.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTerminal : MonoBehaviour
+{
+    //Atributos
+
+    private GameObject pantalla;
+    private GameObject camara;
+    private Vector3 direccionPantalla;
+    private Quaternion cuaternion;
+    private float giroY;
+    private float giroX;
+    private Vector2 orientacion_camera_y;
+    private Vector2 orientacion_camera_x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pantalla = GameObject.Find("Pantalla");
+        camara = GameObject.Find("Main Camera");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direccionPantalla = (camara.transform.position - pantalla.transform.position);
+        orientacion_camera_y = new Vector2(direccionPantalla[0], direccionPantalla[2]);
+        orientacion_camera_x = new Vector2(direccionPantalla[1], direccionPantalla[2]);
+        giroY = Vector2.Angle(orientacion_camera_y,new Vector2(0, -1));
+
+        if(camara.transform.position.x<=this.transform.position.x){
+            giroY = giroY;
+        }
+        else{
+            giroY = -giroY;
+        }
+
+        giroX = Vector3.Angle(direccionPantalla, new Vector3(0,1,0));
+        pantalla.transform.eulerAngles = new Vector3(-giroX, giroY, 0);
+
+    }
+}
