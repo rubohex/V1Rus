e92f253c56eb82bacdb146a799973a4a0cd0c8ca
marioazcona <60815100+mazcona333@users.noreply.github.com>
2020-02-15 14:23:30 +0100

    Inicio hackeo

---
+++ V1Rus/Assets/Script/Behaviour/BHackeo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BHackeo : MonoBehaviour
+{
+    public float tHack;
+    private float t = 0;
+    //private bool hackeado = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.X)/* && t <= tHack */)
+        {
+            t += Time.deltaTime;
+            if(t >= tHack)
+            {
+                //hackeado = true;
+                print("HACKEADOWO");
+            }
+        }
+        else
+        {
+            t = 0;
+        }
+    }
+    /*
+    public void hackeo(BTerminal terminal)
+    {
+
+    }
+    */
+}
