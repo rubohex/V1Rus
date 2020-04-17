2193e7c424424b878295a8c8123c6af45cb586ff
Arkathas <enekozubiaurre@gmail.com>
2020-04-17 13:59:33 +0200

    Inicio en nivel 0
    Que cuando le demos al play, empezemos en el nivel 0

--- V1Rus/Assets/Script/Behaviour/Nivel Interior/BGameManager.cs
+++ V1Rus/Assets/Script/Behaviour/Nivel Interior/BGameManager.cs
@@ -37,7 +37,7 @@ public class BGameManager : MonoBehaviour
     public float maxDisolveTimer;
     public float minDisolveTimer;
 
-    private int iter = 7;
+    private int iter = 0;
 
     #endregion
 
