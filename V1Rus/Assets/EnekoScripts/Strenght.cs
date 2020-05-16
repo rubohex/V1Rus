df4246d94b0208425a998e7c13b2c5741401c2c3
Arkathas <enekozubiaurre@gmail.com>
2020-05-16 19:34:56 +0200

    Ventanas Ejemplo Casi terminado

---
+++ V1Rus/Assets/EnekoScripts/Strenght.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Strenght : MonoBehaviour
+{
+    public int unitValor;
+    public TextMeshProUGUI unitText;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        unitText.text = unitValor.ToString();
+
+
+        if(unitValor>=0 && unitValor < 4)
+        {
+            unitText.color = Color.red;
+
+            
+        }
+        if (unitValor >= 4 && unitValor < 8)
+        {
+            unitText.color = Color.yellow;
+
+
+        }
+        if (unitValor >= 8)
+        {
+            unitText.color = Color.green;
+
+
+        }
+    }
+}
